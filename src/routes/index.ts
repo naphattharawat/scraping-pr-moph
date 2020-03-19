303eed2014523e77b41ee0045d507cdb4a3bc1c5
naphattharawat <naphattharawat@gmail.com>
2020-03-20 01:07:09 +0700

    first commit

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,32 +1,44 @@
 import * as express from 'express';
 import { Router, Request, Response } from 'express';
 import { Jwt } from '../models/jwt';
-
+import * as osmosis from 'osmosis';
 import * as HttpStatus from 'http-status-codes';
 
 const jwt = new Jwt();
 
 const router: Router = Router();
 
+
 router.get('/', (req: Request, res: Response) => {
-  res.send({ ok: true, message: 'Welcome to RESTful api server!', code: HttpStatus.OK });
-});
-
-router.get('/gen-token', async (req: Request, res: Response) => {
-
   try {
-    let payload = {
-      fullname: 'SATIT RIANPIT',
-      username: 'satit',
-      id: 1
-    }
+    let list = [];
+    osmosis
+      .get('https://pr.moph.go.th/?url=pr/index/2/04/')
+      .find('div.comment-widgets > div.comment-row')
+      .set({
+        'img': 'img@src',
+        'link': 'h4 > b > a@href',
+        'title': 'h4 > b > a',
+        'date': 'div.comment-footer > span[1]',
+        'by': 'div.comment-footer > span[2]',
+        'view': 'div.comment-footer > span[3]'
+      })
+      .data(function (data) {
+        list.push(data);
+      })
+      .done(function () {
+        for (const l of list) {
+          l.img = `https://pr.moph.go.th/${l.img}`;
+          l.link = `https://pr.moph.go.th${l.link}`;
+        }
+        res.send({ ok: true, rows: list });
 
-    let token = jwt.signApiKey(payload);
-    res.send({ ok: true, token: token, code: HttpStatus.OK });
+      })
+    // res.send('<h1>Hello Fucking World</h1>');
   } catch (error) {
-    res.send({ ok: false, error: error.message, code: HttpStatus.INTERNAL_SERVER_ERROR });
+    res.send({ ok: false, error: error });
   }
-
 });
+
 
 export default router;
